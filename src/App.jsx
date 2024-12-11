9b32a652f07da3705656dcb82bb53793c8d31b75
riyan ahemed khan <riyanahemedkhangmail.com>
2024-12-11 16:00:50 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import Home from './Components/Home/Home'
-import { Routes,Route } from 'react-router-dom'
-import Login from './Components/Login/Login'
-import Player from './Components/Player/Player'
+import React, { useEffect } from 'react';
+import Home from './Components/Home/Home';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Player from './Components/Player/Player';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user && (window.location.pathname === '/login')) {
+        navigate('/'); // Redirect only from login to home
+      } else if (!user && (window.location.pathname !== '/login')) {
+        navigate('/login'); // Redirect only if not on the login page
+      }
+    });
+    return () => unsubscribe();
+  }, [navigate]);
+
   return (
     <div>
+      <ToastContainer theme='dark' />
       <Routes>
-        <Route path='/' element={ <Home/>}/>
-        <Route path='/login' element={ <Login/>}/>
-        <Route path='player/:id' element={<Player/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player />} />
       </Routes>
-     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 import caret_icon from '../../assets/caret_icon.svg'
+import { logout } from '../../firebase'
 
 
 const Navbar = () => {
@@ -8,7 +9,8 @@ const Navbar = () => {
     <div className='navbar'>
       <div className="improve-logo">Improve</div>
       <div className="goals">GOALS <textarea id="expandingTextarea" placeholder="Start typing..."></textarea></div>
-      <div className="profile"><a>Profile</a><img className='caret-icon' src={caret_icon}  /></div>
+      <div className="profile">
+      <a onClick={()=>{logout()}}>Profile</a></div>
     </div>
   )
 }
